9d40950333d567096a28dc37e4fa9d0e8b63a8cd
dennisn <denonjugush@gmail.com>
2021-02-12 06:37:37 +0200

    Added high level kwargs to the wrapper

--- preprocessor.py
+++ preprocessor.py
@@ -7,27 +7,26 @@ sample_images folder.
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.filters import meijering
 from skimage.filters import threshold_otsu, threshold_mean, threshold_isodata, threshold_li # li: too noisy
-from skimage.filters import meijering, sato, frangi, hessian # ridge filters
+from skimage.filters import meijering, sato, frangi, hessian
 from skimage.morphology import skeletonize, remove_small_holes, remove_small_objects
 from skimage.filters.rank import enhance_contrast
 from skimage import exposure, color, filters, io
 from skimage.transform import rescale, resize, downscale_local_mean
-
 from os import listdir
 
-def pre_process_img(img_file, filter_algo='frangi', sigmas_range=range(1,10,1), min_size=350):
+def pre_process_img(img_file, filter_algo='frangi', sigmas_range=range(1,10,1),
+                    min_size=300, alpha=0.5, beta=0.5, gamma=5, mode='reflect'):
   """Pre-process images.
 
   img_file: image file to be processed
-  filter_algo: ridge detection algorithm
-  sigmas: range of sigmas TODO: Not sure how this is actually applied
+  filter_algo: frangi, meijering, sato, hessian
+  sigmas: range of sigmas
   min_size: minimum size of  objects to be eliminated from a binary image
 
-  Returns a binary image and a relevant cmap
-  parameter(cmap.cm.binary_r) for example can be used to plot the
-  resulting image.
+  Returns a binary image (rather its matrix representation). A
+  relevant cmap parameter(cmap.cm.binary_r) for example can be used to
+  plot the resulting image.
 
   """
   # pick an image
@@ -39,24 +38,26 @@ def pre_process_img(img_file, filter_algo='frangi', sigmas_range=range(1,10,1), 
   p1, p2 = np.percentile(img_rescaled, (2, 98))
   img_enhc = exposure.rescale_intensity(img_rescaled, in_range=(p1, p2))
 
-  # detect ridges
   if filter_algo == 'frangi':
+    # Defaults: alpha=0.5, beta=0.5, gamma=0.5
     print('frangi..')
-    img_ridges = frangi(img_enhc, sigmas=sigmas_range, alpha=0.5,
-                        beta=0.5, gamma=5, black_ridges=True, cval=0)
+    img_ridges = frangi(img_enhc, sigmas=sigmas_range, alpha=alpha,
+                        beta=beta, gamma=gamma, black_ridges=True, cval=0)
   elif filter_algo == 'meijering':
+    # Defaults: alpha=None, mode='reflect'
     print('meijering..')
-    img_ridges = meijering(img_enhc, sigmas=sigmas_range, alpha=None,
-                           black_ridges=True, mode='reflect', cval=0)
+    img_ridges = meijering(img_enhc, sigmas=sigmas_range, alpha=alpha,
+                           black_ridges=True, mode=mode, cval=0)
   elif filter_algo == 'sato':
     print('sato...')
     img_ridges = sato(img_enhc, sigmas=sigmas_range, black_ridges=True,
                       mode=None, cval=0)
   elif filter_algo == 'hessian':
+    # Defaults: alpha=0.5, beta=0.5, gamma=15, mode=None
     print('hessian...')
     img_ridges = hessian(img_enhc, sigmas=sigmas_range, scale_range=None,
-                         scale_step=None, alpha=0.5, beta=0.5, gamma=15,
-                         black_ridges=True, mode=None, cval=0)
+                         scale_step=None, alpha=alpha, beta=beta, gamma=gamma,
+                         black_ridges=True, mode=mode, cval=0)
 
   # binarize image
   thresh = threshold_otsu(img_ridges)
@@ -69,24 +70,25 @@ def pre_process_img(img_file, filter_algo='frangi', sigmas_range=range(1,10,1), 
 
 
 if __name__ == '__main__':
-    # Test code here
+    # Test code
     dir_path = './sample_images/'
     bin_imgs = list()
     orig_imgs = listdir(dir_path)
-    fig, ax = plt.subplots(len(orig_imgs)-1, 2, figsize=(10, 5))
 
+    fig, ax = plt.subplots(len(orig_imgs)-1, 2, figsize=(10, 5))
     for i, f in enumerate(orig_imgs):
         if f.endswith('.jpg'):
             orig_img = io.imread(dir_path+f)
             print(dir_path+f)
             bin_img = pre_process_img(orig_img)
+            # _ = ax[i] = np.hstack((orig_img[600:3000, 200:4400], bin_img))
+            # ax[i,0].set_title(fname[0] + '_' + fname[-2], fontsize=10)
             _ = ax[i,0].imshow(orig_img)
             _ = ax[i,1].imshow(bin_img, cmap=plt.cm.binary_r)
             fname = f.split('_')
             ax[i,0].set_title(fname[0] + '_' + fname[-2] + '_orig', fontsize=10)
             ax[i,1].set_title(fname[0] + '_' + fname[-2] + '_bin', fontsize=10)
-    fig.tight_layout()
-    plt.show()
-
     # for i, (orig_img, bin_img) in enumerate(zip(orig_imgs, bin_imgs)):
     #     print(orig_img.shape, bin_img.shape)
+    fig.tight_layout()
+    plt.show()
