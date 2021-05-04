ad356f1a7bba9998719a3f18d4d3643c56c9b789
cubasja <40700696+cubasja@users.noreply.github.com>
2021-05-04 15:51:50 +0300

    added main()
    Script checks amount of parameters, input tip size. If image doesn't have tips, execution stops

--- Root tips/scenario1.py
+++ Root tips/scenario1.py
@@ -7,63 +7,99 @@
 from fileWITHfunctions import find_contours, processImage, draw_circles_around, add_Text, process_ML_image
 
 import pandas as pd
+import numpy as np
 import sys
 import cv2
-
-# The first image is the processed image after the root segmentation
-# The second image is the original image for the root tip drawing
-im_s = sys.argv[1]
-im_o = sys.argv[2]
-
-# Size of the root tip for analysis1.py 
-# 1 pixel = 0,1 mm
-tip_size = int(sys.argv[3])/0.1
-
-print(f'Searching tips with diameter over: {int(tip_size)}px')
 
 """ 
 STEP 1:
 Read the image as unchanged 1-channel gray image and convert it to binary
 
 STEP 2:
-Open original color image and resize it.
+Find contours and collect root tip information into the dictionary
 
 STEP 3: 
-Find contours and collect root tip information into the dictionary. 
+Open original color image, resize it and draw found tips
 
 STEP 4: 
-Create dataframe, draw contours and save root tip information to csv-file
-
-STEP 5:
-Save image with marked tips
+Create dataframe and save it to csv-file
 """
-im_binary, im_gray = process_ML_image(im_s)
+def main(im_s, im_o, tip_size):
+    im_binary, im_gray = process_ML_image(im_s)
 
-# Process original color image for visualization
-im_original = processImage(im_o)
+    # Process original color image for visualization
+    im_original = processImage(im_o)
+    cv2.imwrite('RESIZED color.png', im_original)
 
-# Find countours from the binary image
-im_COORDINATES = find_contours(im_s, im_binary, im_gray, tip_size)
+    # Calculate binary image colors, if roots exist it should be 2
+    unique, counts = np.unique(im_binary, return_counts=True)
+    image_colors = len(dict(zip(unique, counts)))
 
-# Create the dataframe from dictionary 
-image_df = pd.DataFrame.from_dict(im_COORDINATES, orient='index', columns=['Image','(X,Y) coordinates', 'Radius','Diameter'])
+    if image_colors == 2:
+        # Find countours from the binary image
+        im_COORDINATES = find_contours(im_s, im_binary, im_gray, tip_size)
+        if bool(im_COORDINATES): 
+            # Create dataframe from dictionary 
+            image_df = pd.DataFrame.from_dict(im_COORDINATES, orient='index', columns=['Image','(X,Y) coordinates', 'Radius','Diameter'])
 
-# Draw circles around root tips
-# Circle and text color is green
-color = (0,255,0)
-draw_circles_around(im_original, list(image_df['(X,Y) coordinates']),list(image_df['Radius']), color)
+            # Draw circles around root tips
+            # Circle and text color is green
+            color = (0,255,0)
+            draw_circles_around(im_original, list(image_df['(X,Y) coordinates']),list(image_df['Radius']), color)
+            draw_circles_around(im_binary, list(image_df['(X,Y) coordinates']),list(image_df['Radius']), (255,255,255))
+            draw_circles_around(im_binary, list(image_df['(X,Y) coordinates']),list(image_df['Radius']), (255,255,255))
 
-# Identify root tips
-add_Text(im_original, list(image_df['(X,Y) coordinates']), list(image_df['Radius']), color)
 
-# Generate csv-file name
-csv_file = im_s.split('.')[0] + '.csv'
+            # Identify root tips
+            add_Text(im_original, list(image_df['(X,Y) coordinates']), list(image_df['Radius']), color)
 
-# Reindex dataframe 
-index_list = range(1,len(im_COORDINATES)+1)
-image_df['ImageID']=index_list
-image_df.to_csv(csv_file, sep=';', index=False)
+            # Generate csv-file
+            csv_file = im_s.split('.')[0] + '.csv'
 
-# Save processed image
-png_file = im_s.split('.')[0] + '.png'
-cv2.imwrite(png_file, im_original)
+            # Reindex dataframe 
+            index_list = range(1,len(im_COORDINATES)+1)
+            image_df['ImageID']=index_list
+            image_df['Tip length, mm']=image_df['Diameter']*0.1
+            image_df[['Image','(X,Y) coordinates', 'ImageID', 'Tip length, mm']].to_csv(csv_file, sep=';', index=False, float_format='%.1f')
+
+            # Save processed image
+            png_file = im_s.split('-')[0] + '.png'
+            cv2.imwrite(png_file, im_original)
+            cv2.imwrite('im_binary.png', im_binary)
+            print(f'Image with identified root tips is created, number of roots: {len(im_COORDINATES)}')
+
+        else:
+            print("No root tips")
+
+    else:
+        print("Empty image, no root tips")
+
+if __name__ == "__main__":
+
+    if len(sys.argv)==4:
+        # The first image is the processed image after the root segmentation
+        # The second image is the original image for the root tip drawing
+        im_s = sys.argv[1]
+        im_o = sys.argv[2]
+
+        # Size of the root tip for analysis1.py 
+        # 1 pixel = 0,1 mm
+        tip_size = int(sys.argv[3])/0.1
+
+        if tip_size>1:
+            print(f'Searching tips with diameter over: {int(tip_size)}px')
+            main(im_s, im_o, tip_size)
+        else:
+            print("Root tip size is too small")
+    else:
+        print("Root tip size or filename is missing")
+
+
+
+
+
+
+
+
+
+
